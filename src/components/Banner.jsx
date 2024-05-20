cb74fbdd368bef65f752b3011f2adb283c19055f
devRezaulkarim <dev.rezaulkarim@gmail.com>
2024-05-20 05:06:10 -0500

    FEAT: Banner carousel adeed with react slick package

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,44 @@
 import Button from "./Button";
 import bannerImage from "../assets/hero.png";
 
-const Banner = () => {
-  return (
-    <div className="bg-[#eaeaea] ">
-      <div className=" max-w-[1321px] mx-auto h-[918px] flex items-center bg-no-repeat bg-center">
-        <div className={`w-full grid place-content-end mt-auto pr-[149px]`}>
-          <img src={bannerImage} alt="Banner Image" />
-        </div>
+import Slider from "react-slick";
 
-        <div className="absolute">
-          <h1 className="font-josefin text-5xl font-bold w-[556px] mb-[52px]">
-            Personalized Eyeglass Shopping
-          </h1>
-          <Button icon={true}>Buy Now</Button>
-        </div>
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+  const bannerSlides = Array.from({ length: 3 });
+  return (
+    <div className="bg-[#eaeaea] banner">
+      <div className="container mx-auto h-[918px]">
+        <Slider className="" {...settings}>
+          {bannerSlides.map((n, i) => (
+            <div key={i} className="h-[918px]">
+              <div className="bg-no-repeat h-full relative bg-bottom w-[85%] mx-auto">
+                <div
+                  className={`max-w-[1321px] ml-auto grid place-content-end h-full mt-auto px-[149px]`}
+                >
+                  <img src={bannerImage} alt="Banner Image" />
+                </div>
+
+                <div className="absolute left-0 top-1/2 -translate-y-1/2">
+                  <h1 className="font-josefin text-5xl font-bold w-[556px] mb-[52px]">
+                    Personalized Eyeglass Shopping
+                  </h1>
+                  <Button icon={true}>Buy Now</Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable react/prop-types */
 import { FaRegHeart } from "react-icons/fa6";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductCard = ({
   product: { img, title, previousPrice, currentPrice, discount },
 }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
   return (
-    <div className="font-josefin h-[636px] hover:-translate-y-5 duration-200 relative rounded">
+    <div className="font-josefin h-[636px] hover:-translate-y-5 duration-200 relative rounded ">
       {discount && (
         <span className="text-sm font-bold px-[17px] py-[20px]  rounded-full bg-[#fed29c] absolute top-8 left-8 grid place-items-center">
           {discount}%
@@ -14,8 +26,26 @@ const ProductCard = ({
       <span className="text-2xl p-[15px] border-2 border-black rounded-full absolute top-8 right-8">
         <FaRegHeart />
       </span>
-      <div className="bg-[#f7f7f7] h-[475px] grid place-items-center px-[35px] mb-[30px]">
-        <img src={img} alt="" />
+      <div className="bg-[#f7f7f7] h-[475px]  px-[35px] mb-[30px]">
+        <Slider {...settings}>
+          <div className="h-[430px] w-full">
+            <div className="h-full grid place-items-center">
+              <img className="" src={img} alt="" />
+            </div>
+          </div>
+          <div className="h-[430px] w-full">
+            <div className="h-full grid place-items-center">
+              <img className="" src={img} alt="" />
+            </div>
+          </div>
+          <div className="h-[430px] w-full">
+            <div className="h-full grid place-items-center">
+              <img className="" src={img} alt="" />
+            </div>
+          </div>
+        </Slider>
+
+        {/* <img src={img} alt="" /> */}
       </div>
       <p className="text-3xl">{title}</p>
       <p className="mt-[10px] mb-[20px] text-lg flex items-center gap-2">
