162e5d592bc5c818c9d68db16c1d8ed71c89f76f
devRezaulkarim <dev.rezaulkarim@gmail.com>
2024-05-20 06:13:59 -0500

    FIX: Baaner buton was overped by the slider navigation dots. Now tht's fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -26,15 +26,15 @@ const Banner = () => {
         <Slider {...settings}>
           {bannerSlides.map((n, i) => (
             <div key={i} className="h-[60vh] md:h-[70vh] lg:h-[918px]">
-              <div className="bg-no-repeat h-full relative bg-bottom lg:w-[85%] mx-auto">
+              <div className="bg-no-repeat h-full relative bg-bottom lg:w-[85%] mx-auto ">
                 <div
                   className={`max-w-[1321px] ml-auto grid place-content-end h-full mt-auto lg:px-[149px] px-4`}
                 >
                   <img src={bannerImage} alt="Banner Image" />
                 </div>
 
-                <div className="absolute flex flex-col items-center lg:block lg:left-0  lg:-translate-x-0  bottom-10 md:bottom-14 lg:bottom-1/2 lg:translate-y-1/2 w-full">
-                  <h1 className="font-josefin text-3xl md:text-5xl font-bold  md:w-[556px] mb-4 lg:mb-[52px] text-center lg:text-start">
+                <div className="absolute z-50 flex flex-col items-center lg:block lg:left-0  lg:-translate-x-0  bottom-10 md:bottom-14 lg:bottom-1/2 lg:translate-y-1/2 w-full">
+                  <h1 className="font-josefin text-3xl md:text-5xl lg:text-4xl xl:text-5xl font-bold  md:w-[556px] lg:w-[456px] xl:w-[556px] mb-4 lg:mb-[52px] text-center lg:text-start">
                     Personalized Eyeglass Shopping
                   </h1>
                   <Button icon={true}>Buy Now</Button>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import bravo from "../assets/Bravo-Sunglasses-product.png";
 import vincent from "../assets/Vincent-Chase-product.png";
 import john from "../assets/John-Jacobs-product.png";
@@ -9,6 +10,7 @@ import ProductCard from "./ProductCard";
 
 const Products = () => {
   const [selectedCategory, setSelectedCategory] = useState("Special");
+  const [showingProducts, setShowingProducts] = useState([]);
   const productCategories = ["Latest", "Special", "Best sell"];
   const products = [
     {
@@ -56,7 +58,26 @@ const Products = () => {
       img: occhiali,
     },
   ];
-
+  useEffect(() => {
+    if (selectedCategory === "Latest") {
+      console.log("first");
+      setShowingProducts(products.reverse());
+    } else if (selectedCategory === "Best sell") {
+      setShowingProducts(
+        products.sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        })
+      );
+    } else {
+      setShowingProducts(products);
+    }
+  }, [selectedCategory]);
   return (
     <div className="max-w-[1321px] mx-auto py-[150px] font-josefin px-4">
       <h1 className="text-4xl font-bold mb-5">Our Products</h1>
@@ -69,6 +90,7 @@ const Products = () => {
         <div className="flex items-center mt-8 md:mt-0 justify-center">
           {productCategories.map((cat) => (
             <button
+              onClick={() => setSelectedCategory(cat)}
               className={`rounded border px-5 py-1 duration 200 whitespace-nowrap md:text-xl lg:text-base ${
                 selectedCategory === cat ? "border-black" : "border-transparent"
               }`}
@@ -81,7 +103,7 @@ const Products = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[35px]">
-        {products.map((product) => (
+        {showingProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
